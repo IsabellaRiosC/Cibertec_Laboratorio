5748d0694f74b3ff84f50af7c63ceea48db2fc83
Luis Enrique <luchoarrieta@hotmail.com>
2016-08-14 13:37:05 -0500

    Uso Helpers

--- WebDeveloper/Controllers/HomeController.cs
+++ WebDeveloper/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebDeveloper.Filters;
+
 
 namespace WebDeveloper.Controllers
 {
+    [ExceptionControl]
     public class HomeController : Controller
     {
+    
         public ActionResult Index()
         {
             return View();
