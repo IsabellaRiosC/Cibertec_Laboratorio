7229a6a68b7ca251a232cadadd5a14346dabcba9
Isabella Rios <isabellarios2614@gmail.com>
2016-09-25 13:59:34 -0500

    Codigo Angular

--- WebDeveloper/App_Start/BundleConfig.cs
+++ WebDeveloper/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace WebDeveloper
         // Para obtener más información sobre Bundles, visite http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
